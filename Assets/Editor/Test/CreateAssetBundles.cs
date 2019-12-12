31565964d63b2c1fb3cb51bfc8591c9b3004adb9
May25th <214816701.com>
2019-12-12 18:06:21 +0800

    add

---
+++ Assets/Editor/Test/CreateAssetBundles.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles:Editor
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string dir = "AssetBundles";
+        if (Directory.Exists(dir) == false)
+        {
+            Directory.CreateDirectory(dir);
+        }
+        //BuildTarget 选择build出来的AB包要使用的平台
+        /*
+            BuildAssetBundleOptions.None LZMA算法压缩，压缩的包更小，但是加载时间更长
+            BuildAssetBundleOptions.UncompressedAssetBundle：不压缩，包大，加载快
+            BuildAssetBundleOptions.ChunkBasedCompression：使用LZ4压缩，压缩率没有LZMA高，但是我们可以加载指定资源而不用解压全部
+        */
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
---
+++ Assets/Scripts/May25th/ToolKit/AssetBundle/LoadFromLocal.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadFromLocal : MonoBehaviour
+{
+    private void Start()
+    {
+        //TestDiqiu();
+        //StartCoroutine("Load");
+        StartCoroutine(WWWLoad("http://bi.bellcat.cn/package/AssetBundles/test/fangkuai"));
+    }
+
+    private void TestDiqiu()
+    {
+        //加载对应材质球的依赖
+        AssetBundle ab1 = AssetBundle.LoadFromFile("AssetBundles/ziyuan/ziyuan_01");
+        AssetBundle ab = AssetBundle.LoadFromFile("AssetBundles/test/fangkuai");
+        if (ab == null) {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        GameObject obj = ab.LoadAsset<GameObject>("Diqiu");
+        Instantiate(obj);
+    }
+
+    private void TestFangkuai()
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile("AssetBundles/test/fangkuai");
+        if (ab == null) {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        GameObject obj = ab.LoadAsset<GameObject>("Fangkuai");
+        Instantiate(obj);
+    }
+
+    //使用协程来进行异步加载
+    IEnumerator Load()
+    { 
+        //异步加载，返回一个结果 
+        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync("AssetBundles/test/fangkuai"); 
+        yield return request; 
+        AssetBundle ab = request.assetBundle; 
+        //获取目标对象 
+        GameObject go = ab.LoadAsset<GameObject>("Fangkuai");
+        //实例化对象 
+        Instantiate(go);
+    }
+
+    //使用这种方式加载，它会先把资源包存在本地的cache中
+    //然后在从cache中加载ab包
+    IEnumerator WWWLoad(string path)
+    {
+        //cache是否准备好
+        while(Caching.ready==false){
+            yield return null;
+        }
+        //根据路径加载，可以是网上的路径，也可以是本地路径
+        //UnityWebRequest request = UnityWebRequest.GetAssetBundle(path);
+        WWW www = new WWW(path);
+        yield return www;
+        if(www.error!=null){
+            Debug.Log(www.error);
+            yield break;
+        }
+        AssetBundle ab = www.assetBundle;
+        //获取目标对象
+        GameObject go = ab.LoadAsset<GameObject>("Fangkuai");
+        //实例化对象
+        Instantiate(go);
+    }
+}
